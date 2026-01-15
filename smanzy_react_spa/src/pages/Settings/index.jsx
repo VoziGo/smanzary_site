71142ffee5ac04ef575560243d0fb4de28f548cc
ristep <ristepan@gmail.com>
2026-01-15 01:43:12 +0100

    feat: update Settings page with Panel component and improved styling

--- smanzy_react_spa/src/pages/Settings/index.jsx
+++ smanzy_react_spa/src/pages/Settings/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import { Panel } from "@/components";
+import styles from "./index.module.scss";
 
-const Settings = () => {
+export default function Settings() {
     return (
-        <div>
-            <h1>Settings</h1>
+        <div className={styles.container}>
+            <Panel title="Settings">
+                <div>
+                    <h2>Settings</h2>
+                    <ul className={styles.list}>
+                        <li>Under Construction!</li>
+                    </ul>
+                </div>
+            </Panel>
         </div>
     );
-};
-
-export default Settings;   
+}
