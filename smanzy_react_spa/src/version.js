bdba0f1867cb1b1a652573937249323a1df9e899
ristep <ristepan@gmail.com>
2026-01-26 11:09:25 +0100

    Update version.js

--- smanzy_react_spa/src/version.js
+++ smanzy_react_spa/src/version.js
@@ -1,7 +1,7 @@
 export const VERSION_MAJOR = 1;
 export const VERSION_MINOR = 2;
-export const VERSION_PATCH = 0;
-export const VERSION_PRE = "beta";
+export const VERSION_PATCH = 3;
+export const VERSION_PRE = "BugFix";
 
 export const formatVersion = (major, minor, patch, pre) => {
   return `${major}.${minor}.${patch}-${pre}`;
